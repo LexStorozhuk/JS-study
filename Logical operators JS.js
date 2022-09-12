89297938ef6f078787a46c13502083640b6c7408
LexStorozhuk <sasha0878@yahoo.com>
2022-09-12 23:46:08 +0300

    logical operators js

---
+++ Logical operators JS.js
@@ -0,0 +1,51 @@
+let b
+undefined
+'' || 'Alex'
+"Alex"
+'Alex' || 'Bogdan'
+"Alex"
+'Bog' && 'Bogdan'
+"Bogdan"
+let b = 10
+undefined
+b && console.log('Executed!')
+VM978:1 Executed!
+undefined
+let c
+undefined
+c && console.log('Executed!')
+undefined
+'Alex' && ''
+""
+'' && 'Alex'
+""
+'Alex' && 'Bogdan'
+"Bogdan"
+!10
+false
+!0
+true
+!'abc'
+false
+!''
+true
+!true
+false
+!undefined
+true
+!!10
+true
+!!0
+false
+!!'abc'
+true
+!!""
+false
+!!true
+true
+!!undefined
+false
+!!{}
+true
+!{}
+false
