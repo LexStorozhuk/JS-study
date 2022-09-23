f49d7284f8a4f76ee880ef52366487ea543d4f74
LexStorozhuk <sasha0878@yahoo.com>
2022-09-23 17:52:18 +0300

    Instruction if else

---
+++ if else.js
@@ -0,0 +1,38 @@
+let val = 10
+if (val > 5) {
+    val += 20
+}
+console.log(val)
+const person = {
+    age: 20
+}
+if (!person.name) {
+    console.log('Name is not found')
+}
+if (val < 5) {
+    val += 20
+} else {
+    val -= 20
+}
+console.log(val)
+const age = 7
+ if (age > 18) {
+    console.log('Is adult')
+ } else if (age >= 12) {
+    console.log('Is teenager')
+ } else {
+    console.log ('Is child')
+ }
+ const age1 = 19
+ 
+ if (age1 >= 18) {
+    console.log('Is adult')
+ }
+ 
+ if (age1 >= 12 && age1 < 18) {
+    console.log('Is teenager')
+ }
+
+ if (age1 < 12) {
+    console.log ('Is child')
+ }
