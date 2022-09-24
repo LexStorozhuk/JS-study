0ea1ed3b252c7addf7646d210ffc9717862c198e
LexStorozhuk <sasha0878@yahoo.com>
2022-09-24 12:22:49 +0300

    Instruction switch; Ternary operator with error

--- Выражения.js
+++ Выражения.js
@@ -1,10 +1,25 @@
+let a;
+
+const f = 5;
+
+if (a > b) {
+   console.log('a is larger');
+}
+for (let i = 0; i++; i <5) {
+   console.log (i);
+}
 function myFn (a) {
     console.log(a);
 }
-const b = true;
+const g = true
+
+function myFn (a) {
+    console.log(a);
+}
+const h = true;
 let c = 10;
 myFn(2+3)
 myFn(b)
 myFn(c = c + 1)
 myFn(c = c + 1;)
-myFn(let d)
+myFn(let d) 
